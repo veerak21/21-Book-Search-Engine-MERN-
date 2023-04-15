3f5c024f90dda2137419ed2f50cbcce45b6dfa2b
C M Santosh Kumar <pagadala.kalyani@gmail.com>
2023-04-15 15:30:38 +0100

    ApolloProvider created

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,39 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+// import ApolloProvider destructured  from apollo
+import { ApolloProvider } from "@apollo/react-hooks";
+// import the apollo client from apollo boost
+import ApolloClient from "apollo-boost";
+
+// assign in client to a new apollo client in high order function that takes request as a first are than a function then an operation set context with the trinary to check for the graphql uri
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+});
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
